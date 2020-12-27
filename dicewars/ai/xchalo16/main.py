c3e7c04c4c2c46860d6515983f3d9af391dadefb
Jan Chaloupka <osobni@janchaloupka.cz>
2020-12-28 00:58:42 +0100

    Oprava spouštění (chybějící importy)

--- dicewars/ai/xchalo16/main.py
+++ dicewars/ai/xchalo16/main.py
@@ -1,13 +1,11 @@
 import logging
-import random
 import copy
-
-from ..utils import possible_attacks, save_state
-from .utils import best_sdc_attack, is_acceptable_sdc_attack
+from typing import List
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 from dicewars.client.game.board import Board
 from dicewars.client.game.area import Area
+from .utils import simulate_attack, get_attackable
 
 class AI:
     """GOD player agent
@@ -15,21 +13,14 @@ class AI:
     This agent wins everything
     """
 
-    def __init__(self, player_name: str, board: board.Board, players_order):
-        """
-        Parameters
-        ----------
-        game : Game
-        """
-
+    def __init__(self, player_name: int, board: Board, players_order: List[int]):
         self.player_name = player_name
         self.players_order = players_order
         self.logger = logging.getLogger('AI')
-
         self.first_attack = True
         self.area_of_interest = None
 
-    def search_tree(board : Board, active_area : Area):
+    def search_tree(self, board: Board, active_area: Area):
         """ Recursive function for tree search
         """
         possible_targets = get_attackable(active_area, active_area.get_adjacent_areas())
@@ -47,7 +38,7 @@ class AI:
                     # Board update
                     n_board = simulate_attack(board, active_area, target)
                     # Search tree with updated board
-                    r_paths = search_tree(n_board, n_board.get_area(active_area.get_name()))
+                    r_paths = self.search_tree(n_board, n_board.get_area(active_area.get_name()))
 
                     for path in r_paths:
                         path.insert(0, active_area.get_name())
@@ -58,7 +49,7 @@ class AI:
         if node:
             return [ [active_area.get_name()] ]
 
-    def ai_turn(self, board: board.Board, nb_moves_this_turn, nb_turns_this_game, time_left):
+    def ai_turn(self, board: Board, nb_moves_this_turn: int, nb_turns_this_game: int, time_left: float):
         """ GOD AI agent's turn
 
         TODO At start get player area border area with most dices. Otherwise last used.
@@ -81,7 +72,7 @@ class AI:
         else:
             start_area = self.area_of_interest
 
-        paths = search_tree(board, start_area)
+        paths = self.search_tree(board, start_area)
 
         if not paths:
             self.first_attack = True
