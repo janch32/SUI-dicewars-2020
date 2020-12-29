8088656abadf71b57f3ac4cbd9bd6c340ca8c60e
Jan Chaloupka <osobni@janchaloupka.cz>
2020-12-29 17:59:04 +0100

    Odstranění starého kódu

--- dicewars/ai/xchalo16/maxn.py
+++ dicewars/ai/xchalo16/maxn.py
@@ -1,72 +1,8 @@
-import random
-from ..utils import possible_attacks, attack_succcess_probability, probability_of_holding_area
-from .utils import simulate_battle
-from typing import Dict, List, Tuple, Union
+from ..utils import possible_attacks
+from .utils import add_dices_to_player, battle_heuristic, player_heuristic, simulate_battle
+from typing import List, Tuple, Union
 from dicewars.client.game.board import Board
 from dicewars.client.game.area import Area
-from contextlib import contextmanager
-
-def player_heuristic(board: Board, player_name: int) -> int:
-    """
-    koeficient = počet_polí_hlavního_území
-    """
-    score = 0
-    regions = board.get_players_regions(player_name)
-    for region in regions:
-        score = max(score, len(region))
-    return score
-
-def battle_heuristic(board: Board, attacker: Area, target: Area) -> float:
-    """
-    heuristika = pst_úspěchu * koeficient(úspěch) - pst_prohry * koeficient(prohra)
-    """
-
-    with simulate_battle(attacker, target, success=True):
-        succ_coef = player_heuristic(board, attacker.get_owner_name())
-
-    with simulate_battle(attacker, target, success=False):
-        fail_coef = player_heuristic(board, attacker.get_owner_name())
-
-    succ_prob = attack_succcess_probability(attacker.get_dice(), target.get_dice())
-    hold_prob = probability_of_holding_area(board, target.get_name(), attacker.get_dice()-1, attacker.get_owner_name())
-    fail_hold_prob = probability_of_holding_area(board, attacker.get_name(), 1, attacker.get_owner_name())
-    #return (hold_prob * succ_prob * succ_coef) - (2*(1 - fail_hold_prob) * (1 - succ_prob) * fail_coef)
-    return (hold_prob * succ_prob * succ_coef) - ((1 - succ_prob) * fail_coef)
-
-
-@contextmanager
-def add_dices_to_player(board: Board, player_name: int):
-    affected_areas: Dict[int, int] = {}
-    dice = 0
-    regions = board.get_players_regions(player_name)
-    for region in regions:
-        dice = max(dice, len(region))
-
-    areas: List[Area] = []
-    for area in board.get_player_areas(player_name):
-        dice += area.dice
-        areas.append(area)
-
-    if dice > 64:
-        dice = 64
-
-    while dice and areas:
-        area = random.choice(areas)
-        if area.dice >= 8:
-            areas.remove(area)
-        else:
-            if area.name not in affected_areas:
-                affected_areas[area.name] = area.dice
-            area.dice += 1
-            dice -= 1
-
-    try:
-        yield
-    finally:
-        # Vrátit herní pole do původního stavu
-        for name in affected_areas:
-            board.get_area(name).dice = affected_areas[name]
-
 class MaxN:
     """MiniMax implemenetace pro hru více hráčů (Max^n)
     """
--- dicewars/ai/xchalo16/utils.py
+++ dicewars/ai/xchalo16/utils.py
@@ -1,6 +1,8 @@
+import random
+from typing import Dict, List
 from dicewars.client.game.board import Board
 from dicewars.client.game.area import Area
-from ..utils import attack_succcess_probability
+from ..utils import attack_succcess_probability, probability_of_holding_area
 from contextlib import contextmanager
 
 @contextmanager
@@ -45,30 +47,13 @@ def simulate_battle(attacker: Area, target: Area, success=True):
 
 def player_heuristic(board: Board, player_name: int) -> int:
     """
-    koeficient = (počet_polí_hlavního_území - napadnutelná_pole - nepřímo_napadnutelná_pole)
+    koeficient = počet_polí_hlavního_území
     """
     score = 0
-    attackable_areas = 0
-    p_attackable_areas = 0 # TODO
-
     regions = board.get_players_regions(player_name)
     for region in regions:
         score = max(score, len(region))
-
-        for area_id in region:
-            area: Area = board.get_area(area_id)
-            area_dice = area.get_dice()
-            neigh = area.get_adjacent_areas()
-
-            for nid in neigh:
-                n: Area = board.get_area(nid)
-                if n.get_owner_name() != player_name and n.get_dice() > area_dice:
-                    attackable_areas += 1
-                    break # TODO koeficient když o to můžeme přijít jako fakt hodně
-
-
-    return score - attackable_areas - p_attackable_areas
-
+    return score
 
 def battle_heuristic(board: Board, attacker: Area, target: Area) -> float:
     """
@@ -82,7 +67,44 @@ def battle_heuristic(board: Board, attacker: Area, target: Area) -> float:
         fail_coef = player_heuristic(board, attacker.get_owner_name())
 
     succ_prob = attack_succcess_probability(attacker.get_dice(), target.get_dice())
-    return (succ_prob * succ_coef) - (2 * (1 - succ_prob) * fail_coef)
+    hold_prob = probability_of_holding_area(board, target.get_name(), attacker.get_dice()-1, attacker.get_owner_name())
+    #fail_hold_prob = probability_of_holding_area(board, attacker.get_name(), 1, attacker.get_owner_name())
+    #return (hold_prob * succ_prob * succ_coef) - (2*(1 - fail_hold_prob) * (1 - succ_prob) * fail_coef)
+    return (hold_prob * succ_prob * succ_coef) - ((1 - succ_prob) * fail_coef)
+
+
+@contextmanager
+def add_dices_to_player(board: Board, player_name: int):
+    affected_areas: Dict[int, int] = {}
+    dice = 0
+    regions = board.get_players_regions(player_name)
+    for region in regions:
+        dice = max(dice, len(region))
+
+    areas: List[Area] = []
+    for area in board.get_player_areas(player_name):
+        dice += area.dice
+        areas.append(area)
+
+    if dice > 64:
+        dice = 64
+
+    while dice and areas:
+        area = random.choice(areas)
+        if area.dice >= 8:
+            areas.remove(area)
+        else:
+            if area.name not in affected_areas:
+                affected_areas[area.name] = area.dice
+            area.dice += 1
+            dice -= 1
+
+    try:
+        yield
+    finally:
+        # Vrátit herní pole do původního stavu
+        for name in affected_areas:
+            board.get_area(name).dice = affected_areas[name]
 
 def path_heuristics(board: Board, path: list) -> float:
     """
@@ -99,14 +121,14 @@ def path_heuristics(board: Board, path: list) -> float:
     return h
 
 def get_attackable(board: Board, active_area: Area):
-        neighbors = active_area.get_adjacent_areas()
-        attackable = []
-        for nb in neighbors:
-            a = board.get_area(nb)
-            if active_area.get_owner_name() != a.get_owner_name():
-                attackable.append(a)
+    neighbors = active_area.get_adjacent_areas()
+    attackable = []
+    for nb in neighbors:
+        a = board.get_area(nb)
+        if active_area.get_owner_name() != a.get_owner_name():
+            attackable.append(a)
 
-        return attackable
+    return attackable
 
 # ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╩╜╜╜╜╙╙╜╜║╢╢╢╢╫╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬
 # ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╝╜╙└└││││││╙╜║╢╢╢╢╢╣╢╢╣╢╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬
